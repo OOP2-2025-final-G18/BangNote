871f45bc76221245c031e63f6a43bcebd32f22c2
uiro0120 <k24009kk@aitech.ac.jp>
2026-01-08 13:41:14 +0900

    Note_background
    色のボタンを追加すればノートの背景色が変わるようした(ボタン実装は別ブランチ)

--- frontend/components/NoteDetail.js
+++ frontend/components/NoteDetail.js
@@ -3,17 +3,28 @@ export class NoteDetail extends HTMLElement {
     super();
     this.attachShadow({ mode: "open" });
   }
-
+ 
   connectedCallback() {
     this.render();
+ 
+    document.addEventListener("color-change", (e) => {
+      this.changeColor(e.detail.color);
+    });
   }
-
+ 
   render() {
     this.shadowRoot.innerHTML = `
       <link rel="stylesheet" href="./components/NoteDetail.css">
       <textarea placeholder="メモを入力..."></textarea>
     `;
   }
+ 
+  changeColor(color) {
+    const textarea = this.shadowRoot.querySelector("textarea");
+ 
+    textarea.classList.remove("white", "red", "blue", "green");
+    textarea.classList.add(color);
+  }
 }
-
+ 
 customElements.define("note-detail", NoteDetail);
